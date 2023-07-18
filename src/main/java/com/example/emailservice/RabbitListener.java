209b3c0bb57b63bf9836798c0ae5b8b41aca1569
yiheyoung <47635610+yiheyoung@users.noreply.github.com>
2023-07-17 23:35:22 -0400

    second version of email sending

--- src/main/java/com/example/emailservice/RabbitListener.java
+++ src/main/java/com/example/emailservice/RabbitListener.java
@@ -5,24 +5,45 @@ import org.springframework.amqp.core.MessageListener;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 @Service
 public class RabbitListener implements MessageListener {
+    private ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void onMessage(Message message) {
+
         System.out.println("New message received from "
                 + message.getMessageProperties().getConsumerQueue()
                 + ": "
                 + new String(message.getBody()));
 
+        String email = "yihey@andrew.cmu.edu";
+        String code = "123456";
+
+        try {
+            String body = new String(message.getBody(), StandardCharsets.UTF_8);
+            JsonNode jsonNode = objectMapper.readTree(body);
+
+            email = jsonNode.get("email").asText();
+            code = jsonNode.get("code").asText();
+
+        } catch (IOException e) {
+            // Handle JSON parsing error
+            System.out.println("There's some parse error");
+        }
+
         // sending email
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setFrom("noreply@baeldung.com");
-        mailMessage.setTo("yihey@andrew.cmu.edu");
+        mailMessage.setTo(email);
         mailMessage.setSubject("Verification Code");
-        mailMessage.setText("Your verification code is: 1234");
+        mailMessage.setText("Your verification code is: " + code);
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
